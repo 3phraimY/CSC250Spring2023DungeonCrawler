ee1a8e88a43341f9bb0f37bbc0948c33f476c429
3phraimY <ephraim.young@cuw.edu>
2023-04-05 11:58:13 -0500

    new solution

--- Dungeon Crawler/Assets/Scripts/PlayerController.cs
+++ Dungeon Crawler/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,16 +10,44 @@ public class PlayerController : MonoBehaviour
     public GameObject northExit, southExit, eastExit, westExit;
     public float movementSpeed = 40.0f;
     private bool isMoving;
+    public GameObject westStart;
+    public GameObject southStart;
+    public GameObject eastStart;
+    public GameObject northStart;
     private bool reachedCenter;
 
     // Start is called before the first frame update
     void Start()
     {
         this.rb = this.GetComponent<Rigidbody>();
-        MasterData md = new MasterData();
-        this.isMoving = true;
-        this.reachedCenter = false;
-        if(MasterData.whereDidIComeFrom.Equals("west"))
+        this.isMoving = false;
+        //this.reachedCenter = false;
+        if(!MasterData.whereDidIComeFrom.Equals("?"))
+        {
+            
+            if(MasterData.whereDidIComeFrom.Equals("north"))
+            {
+                this.gameObject.transform.position = this.southStart.transform.position;
+                this.rb.AddForce(Vector3.forward * 150.0f);
+            }
+            else if(MasterData.whereDidIComeFrom.Equals("south"))
+            {
+                this.gameObject.transform.position = this.northStart.transform.position;
+                this.rb.AddForce(Vector3.back * 150.0f);
+            }
+            else if (MasterData.whereDidIComeFrom.Equals("east"))
+            {
+                this.gameObject.transform.position = this.westStart.transform.position;
+                this.rb.AddForce(Vector3.right * 150.0f);
+            }
+            else if (MasterData.whereDidIComeFrom.Equals("west"))
+            {
+                this.gameObject.transform.position = this.eastStart.transform.position;
+                this.rb.AddForce(Vector3.left * 150.0f);
+            }
+            //this.gameObject.transform.position = this.westStart.transform.position;
+        }
+        /*if(MasterData.whereDidIComeFrom.Equals("west"))
         {
             this.transform.position = new Vector3(3.8f, 0.5f, 0);
             this.rb.MovePosition( new Vector3(0, 0.5f, 0));
@@ -34,14 +63,21 @@ public class PlayerController : MonoBehaviour
         if (MasterData.whereDidIComeFrom.Equals("south"))
         {
             this.transform.position = new Vector3(0, 0.5f, 3.8f);
-        }
+        }*/
 
     }
-
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("center"))
+        {
+            this.rb.velocity = Vector3.zero;
+            this.rb.angularVelocity = Vector3.zero;
+        }
+    }
     private void OnTriggerExit(Collider other)
     {
 
-        if (other.gameObject.CompareTag("Exit"))
+        if (other.gameObject.CompareTag("Exit") && MasterData.isExiting)
         {
             if (other.gameObject == this.northExit)
             {
@@ -60,6 +96,10 @@ public class PlayerController : MonoBehaviour
                 MasterData.whereDidIComeFrom = "west";
             }
             SceneManager.LoadScene("DungeonRoom");
+        }
+        else if(other.gameObject.CompareTag("Exit") && !MasterData.isExiting)
+        {
+            MasterData.isExiting = true;
         }
     }
 
@@ -88,7 +128,7 @@ public class PlayerController : MonoBehaviour
             this.isMoving = true;
         }
 
-        if (this.transform.position == new Vector3(0, 0.5f, 0) && !this.reachedCenter && true)
+        /*if (this.transform.position == new Vector3(0, 0.5f, 0) && !this.reachedCenter && true)
         {
             this.rb.velocity = new Vector3(0,0,0);
             this.isMoving = false;
@@ -96,7 +136,7 @@ public class PlayerController : MonoBehaviour
             print("works");
         }
         Vector3 spaceInbetween = new Vector3(0,0,0);
-        if(this.reachedCenter == false && reachedCenter == false)
+        if(this.reachedCenter == false && reachedCenter == false)*/
         {
           //  if(MasterData.whereDidIComeFrom == "east" || MasterData.whereDidIComeFrom == "north")
           //  {
@@ -107,7 +147,7 @@ public class PlayerController : MonoBehaviour
             //    spaceInbetween = this.rb.position + new Vector3(0, 0.5f, 0);
             //}
             //this.rb.MovePosition(this.transform.position + spaceInbetween.normalized * this.movementSpeed * Time.deltaTime);
-            this.rb.MovePosition(new Vector3(0,0.5f,0));
+            //this.rb.MovePosition(new Vector3(0,0.5f,0));
             //this.rb.AddForce(new Vector3(0, 0.5f, 0) * Time.deltaTime);
 
 
